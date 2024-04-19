43bfa3c3db3bc39c5491ae1394365897557b917f
Shubham Jangle <jangleshubham13@gmail.com>
2024-04-19 22:45:31 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 function App() {
   return (
     <div className="App">
-      <div>Hello Coders!</div>
+      <Navbar />
+      <ProductList />
     </div>
   );
 }
 
 export default App;
+
+function Navbar() {
+  return <nav>Hello Coders!</nav>;
+}
+
+function ProductList() {
+  return (
+    <ul>
+      <li style={{ backgroundColor: "orange" }}>Product 1</li>
+      <li id="second">Product 2</li>
+      <li className="bg-primary">Product 3</li>
+      <li>Product 4</li>
+    </ul>
+  );
+}
