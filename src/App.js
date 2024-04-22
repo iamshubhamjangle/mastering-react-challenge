af60679fcf2cdf758eb597605ddd86844d2b6789
Shubham Jangle <jangleshubham13@gmail.com>
2024-04-22 13:38:14 +0530

    Day 4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
+import { useState } from "react";
+
 function App() {
-  const streetFoodList = [
-    { name: "Aloo_Tikki_Chaat", src: "images/Aloo_Tikki_Chaat.png" },
-    { name: "Chole_Bhaturey", src: "images/Chole_Bhaturey.png" },
-    { name: "Dosa", src: "images/Dosa.png" },
-    { name: "Kebab", src: "images/Kebab.png" },
-    { name: "Momos", src: "images/Momos.png" },
-    { name: "Pakoda", src: "images/Pakoda.png" },
-    { name: "Pani_Puri", src: "images/Pani_Puri.png" },
-    { name: "Pav_Bhaji", src: "images/Pav_Bhaji.png" },
-    { name: "Samosa", src: "images/Samosa.png" },
-    { name: "Vada_Pav", src: "images/Vada_Pav.png" },
-  ];
+  const [items, setItem] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleClick = () => {
+    setItem((prev) => [...prev, input]);
+    // alert("Button was clicked");
+  };
 
   return (
     <div className="App">
-      <h1>You would love ❤</h1>
-      {streetFoodList.map((element, index) => {
-        return (
-          <div key={index}>
-            <p>{element.name}</p>
-            <img src={element.src} alt={element.name} />
-          </div>
-        );
-      })}
+      <h3>Day 3: Event handling and State management</h3>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <input
+        placeholder="Enter Item Name"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleClick}>Create</button>
     </div>
   );
 }
