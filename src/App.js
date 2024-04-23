35534bbd515aa91c9e7f66a432973c737747e6eb
Shubham Jangle <jangleshubham13@gmail.com>
2024-04-23 13:24:39 +0530

    Day init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import { useState } from "react";
-
 function App() {
-  const [input, setInput] = useState("");
-  const [items, setItems] = useState([]);
-
-  const handleClick = () => {
-    // alert("Button was clicked" + input);
-    setItems((prev) => [...prev, input]);
-  };
-
   return (
-    <div className="App">
-      <h3>Day 3: Event handling and State management</h3>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Enter a text"
-      />
-      <button onClick={handleClick}>Create</button>
+    <div>
+      <h3>Day 5: API Request & Passing Data</h3>
+      <button>Fetch Data</button>
+      <Post />
     </div>
   );
 }
 
 export default App;
+
+function Post() {
+  return (
+    <div class="card">
+      <div class="img-container">
+        <img
+          src="https://images.unsplash.com/photo-1610932793632-6ba00bdffee8?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTA2NzkwMDh8&ixlib=rb-4.0.3&q=85"
+          width="336"
+          alt=""
+        />
+      </div>
+      <h1>Embracing Minimalism</h1>
+      <p>
+        From minimalist sculptures to minimalist paintings, this blog will
+        inspire you to appreciate the beauty that lies in simplicity.
+      </p>
+    </div>
+  );
+}
